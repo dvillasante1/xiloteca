bcbd42856fdd58032e1956396c4e75fe2b363837
dvillasante <dvillasante@hotmail.com>
2023-05-24 10:39:17 +0200

    Actualización charset utf8mb4 xilotecaDDL.sql

--- xiloteca/DDBB/xilotecaDDL.sql
+++ xiloteca/DDBB/xilotecaDDL.sql
@@ -1,11 +1,9 @@
-CREATE DATABASE  IF NOT EXISTS `xiloteca` 
-/*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  IF NOT EXISTS `xiloteca`;
+
 USE `xiloteca`;
 
-
 DROP TABLE IF EXISTS `muestras`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `muestras` (
   `id_muestra` int NOT NULL AUTO_INCREMENT,
   `cient_acept1` varchar(255) DEFAULT NULL,
@@ -19,5 +17,4 @@ CREATE TABLE `muestras` (
   `muestra_lab` varchar(255) DEFAULT NULL,
   `muestra_mic` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_muestra`)
-) ENGINE=MyISAM AUTO_INCREMENT=502 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=MyISAM AUTO_INCREMENT=502 DEFAULT CHARSET=utf8mb4;
